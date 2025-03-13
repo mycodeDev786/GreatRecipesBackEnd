5212f7566388f72d869dc71a18045f96ef344952
villageprofessor <113996275+mycodeDev786@users.noreply.github.com>
2025-03-13 22:54:51 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
